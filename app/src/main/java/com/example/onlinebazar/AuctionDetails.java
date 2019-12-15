be4fc968c6f7dafc3a30d98a01169432a8e6c125
Dewilek <michal.fiala96@gmail.com>
2019-12-15 21:30:14 +0100

    Addational ifs and work on AucitonDetails class and layout using imaginary DB.

--- app/src/main/java/com/example/onlinebazar/AuctionDetails.java
+++ app/src/main/java/com/example/onlinebazar/AuctionDetails.java
@@ -2,6 +2,9 @@ package com.example.onlinebazar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
@@ -9,6 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AuctionDetails extends AppCompatActivity {
 
@@ -21,6 +25,8 @@ public class AuctionDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auction_details);
+
+
 
         Bundle b = getIntent().getExtras();
         int position = b.getInt("com.example.onlinebazar.ITEM_INDEX");
@@ -41,6 +47,8 @@ public class AuctionDetails extends AppCompatActivity {
         String[] aNameDB = res.getStringArray(R.array.AuctionName);
         String[] abPriceDB = res.getStringArray(R.array.abPrices);
         String[] abPriceGDB = res.getStringArray(R.array.abPricesG);
+        String[] aDescDB = res.getStringArray(R.array.AuctionDesc);
+        String[] aEmailDB = res.getStringArray(R.array.emails);
 
         textView_aName.setText(aNameDB[position]);
         textView_abPrice.setText(abPriceDB[position]);
@@ -51,6 +59,17 @@ public class AuctionDetails extends AppCompatActivity {
         else {
             textView_abPriceGrey.setText(abPriceGDB[position]);
         }
+        if (abPriceGDB[position].equals("NULL"))
+        {
+            btn1.setEnabled(false);
+        }
+
+        textView_desc.setText(aDescDB[position]);
+        String[] phonesDB = res.getStringArray(R.array.phones);
+
+        btn3.setText(phonesDB[position]);
+
+        //String formatted = getString(R.string.phoneNumber,phonesDB[position]);
 
 
 
@@ -72,7 +91,7 @@ public class AuctionDetails extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent callIntent = new Intent(Intent.ACTION_DIAL);
-                callIntent.setData(Uri.parse("tel:"+880217769));//change the number
+                callIntent.setData(Uri.parse("tel:"+phonesDB[position]));
                 startActivity(callIntent);
 
             }
@@ -81,6 +100,10 @@ public class AuctionDetails extends AppCompatActivity {
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("Email",aEmailDB[position]);
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(AuctionDetails.this, "Email " + aEmailDB[position] + " was copied to your clipboard.", Toast.LENGTH_SHORT).show();
 
             }
         });
